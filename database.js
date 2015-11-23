33da0e9540ffb1079a510cddceb6c53acdff8009
Dominic Muttel <dominic.muttel@gmail.com>
2015-11-23 15:23:50 -0700

    added routes

--- database.js
+++ database.js
@@ -1,13 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var user = new Schema ({
+var userSchema = new Schema ({
   firstName: String,
   lastName: String,
   email: String,
+  places: Array,
   newsUpdates: Boolean
 });
 
-var User = mongoose.model('items', itemSchema);
+// var placesSchema = new Schema ({
+
+// });
+
+var User = mongoose.model('users', userSchema);
+
+mongoose.connect('mongodb://localhost/mongo-user');
 
 module.exports = User;
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../../../database.js');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/users', function(req, res, next) {
+  User.find({}, function(err, data){
+    if (err) {
+      res.json(err);
+    }
+    else if (data.length === 0) {
+      res.json({message: 'There are no users in the database'});
+    }
+    else {
+      res.json(data);
+    }
+  });
+});
+
+router.post('/users', function (req, res, next) {
+  var newUser = new User({firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, places: req.body.places, newsUpdates: req.body.newsUpdates});
+  newUser.save(function(err, data) {
+    if (err) {
+      res.json(err);
+    }
+    else {
+      res.json(data);
+    }
+  });
+});
+
+router.put('/users/:id', function(req, res, next) {
+  var id = {_id: req.params.id};
+  var update = {firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, places: req.param.places, newsUpdates: req.body.email};
+  var options = {new: true};
+
+  User.findOneAndUpdate(id, update, options, function(err, data){
+    if (err) {
+      res.json(err.message);
+    }
+    else {
+      res.json(data);
+    }
+  });
+});
+
+router.delete('/users/:id', function(req, res, next) {
+  User.findOneAndRemove({_id: req.params.id}, function(err, data){
+    if (err) {
+      res.json(err.message);
+    }
+    else if (data.length === 0) {
+      res.json({message: 'A user with that ID does not exist in this database.'});
+    }
+    else {
+      res.json({message: 'User has been succesfully deleted.'});
+    }
+  });
+});
+
+
 
 module.exports = router;
